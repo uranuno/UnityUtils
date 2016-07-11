c6db6665c74e1096ab2b810f059e6fa7ecb2c760
yumi nunoura <uranuno@gmail.com>
2016-07-11 17:23:07 +0900

    Add PlayerPrefsData

---
+++ Assets/Utils/PlayerPrefsData.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// PlayerPrefsに保存したいデータのベースクラス。
+/// Json文字列化して保存する。
+/// 保存キー = 型名になるので、1クラス1データ
+/// </summary>
+[System.Serializable]
+public abstract class PlayerPrefsData<T> where T : PlayerPrefsData<T>, new()
+{
+	/// <summary>
+	/// 保存キー取得
+	/// </summary>
+	static string GetKey ()
+	{
+		return typeof(T).Name;
+	}
+
+	/// <summary>
+	/// すでに保存されているか
+	/// </summary>
+	public static bool isSaved
+	{
+		get { return PlayerPrefs.HasKey (GetKey ()); }
+	}
+
+	/// <summary>
+	/// 保存データの読み込み（データが無い場合は新規データを生成し保存する）
+	/// </summary>
+	public static T Load ()
+	{
+		T loaded = new T();
+		if (!isSaved)
+			loaded.Save ();//新規データ保存
+		else
+		{
+			var json = PlayerPrefs.GetString (GetKey ());
+			JsonUtility.FromJsonOverwrite (json, loaded);
+		}
+		return loaded;
+	}
+
+	/// <summary>
+	/// データの保存
+	/// </summary>
+	public void Save ()
+	{
+		var json = JsonUtility.ToJson (this);
+		PlayerPrefs.SetString (GetKey (), json);
+		PlayerPrefs.Save ();
+	}
+}
