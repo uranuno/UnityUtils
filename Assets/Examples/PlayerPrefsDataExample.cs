e5580e7ae908e71e26f4e639066a0a2a96a47e00
yumi nunoura <uranuno@gmail.com>
2017-04-10 18:08:14 +0900

    PlayerPrefsData のサンプルコード追加

---
+++ Assets/Examples/PlayerPrefsDataExample.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// 端末に保存したいデータのクラス（例: 音量の設定）
+public class AudioSettings
+{
+	public float bgmVolume = 1f;
+	public float seVolume  = 1f;
+}
+
+// 使い方
+public class PlayerPrefsDataExample : MonoBehaviour
+{
+	[SerializeField] Slider m_BgmVolumeSlider;
+	[SerializeField] Slider m_SeVolumeSlider;
+	[SerializeField] Button m_ResetButton;
+
+	void Start ()
+	{
+		m_ResetButton.onClick.AddListener (ResetData);
+	}
+
+	void OnEnable  () { LoadData (); }
+	void OnDisable () { SaveData (); }
+	void OnApplicationPause (bool pause)
+	{
+		if (pause) SaveData ();
+	}
+
+	void LoadData ()
+	{
+		// データの読込
+		var data = PlayerPrefsData.Load<AudioSettings> (
+			//データが未保存の場合のデフォルト値
+			new AudioSettings ()
+		);
+		// UIに反映
+		m_BgmVolumeSlider.value = data.bgmVolume;
+		m_SeVolumeSlider.value = data.seVolume;
+	}
+
+	void SaveData ()
+	{
+		// 新規データを作成
+		var data = new AudioSettings ();
+		// UIから値を取得
+		data.bgmVolume = m_BgmVolumeSlider.value;
+		data.seVolume = m_SeVolumeSlider.value;
+		// データの保存
+		PlayerPrefsData.Save<AudioSettings> (data);
+	}
+
+	// データのリセット
+	void ResetData ()
+	{
+		// データの削除
+		PlayerPrefsData.Delete<AudioSettings> ();
+		// データの再読み込み（デフォルト値になる）
+		LoadData ();
+	}
+}
