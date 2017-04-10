835e68ec485f89e0c7b27eeaa8accab14cedf5d5
yumi nunoura <uranuno@gmail.com>
2017-04-10 19:07:39 +0900

    キャプチャがすぐ撮れるように

---
+++ Assets/Editor/Utils.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class Utils
+{
+	[MenuItem ("Utils/Capture")]
+	public static void Capture ()
+	{
+		var fileName = EditorUtility.SaveFilePanel (
+			"Choose Location of the Capture",
+			Application.dataPath,
+			System.DateTime.Now.ToString ("yyyyMMdd-HHmmss-fff"),
+			"png"
+		);
+		if (fileName.Length == 0)
+			return;
+
+		Application.CaptureScreenshot (fileName);
+	}
+}
